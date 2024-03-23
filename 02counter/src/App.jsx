a6c9fec30ac3e35cdabc6c95e480658c314a11d0
shivam8512345 <shivroy851@gmail.com>
2024-03-23 16:44:10 +0530

    Counter interview solved

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -7,17 +7,24 @@ function App() {
 
   const [counter, setCounter] = useState(15)
 
-  
+  // let counter = 5.
+
   const addValue = () => {
-    if(counter < 20){
-      setCounter(counter + 1);
-    }
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+    setCounter(prevCounter => prevCounter + 1);
+
   }
   const removeValue = () => {
-    if(counter > 0){
-      setCounter(counter - 1);
+    if (counter > 0) {
+      setCounter(prevCounter => prevCounter - 1);
+      setCounter(prevCounter => prevCounter - 1);
+      setCounter(prevCounter => prevCounter - 1);
+      setCounter(prevCounter => prevCounter - 1);
     }
   }
+
 
   return (
     <>
